a0aefdcae0d22a7b8b893d6bcb03e8bdfd0fb1af
ushigai <usigaifumito@gmail.com>
2022-12-17 19:21:23 +0900

    ✒ 一部修正

--- 6_message_authentication/hmac_example2.py
+++ 6_message_authentication/hmac_example2.py
@@ -11,6 +11,6 @@ opad, ipad = int("5c"*64, 16), int("36"*64, 16)
 okeypad = long_to_bytes((key << (512 - key_len)) ^ opad)
 ikeypad = long_to_bytes((key << (512 - key_len)) ^ ipad)
 
-mac = md5(okeypad + md5(ikeypad + msg).digest())
+mac =
 print(mac.hexdigest())
 # digest = 0x9294727a3638bb1c13f48ef8158bfc9d
